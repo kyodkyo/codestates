7a75d2cfb1b9f87c0a3431bf4477b4c5205d4577
Jina Choi <dev.jina0120@gmail.com>
2022-08-29 15:32:14 +0900

    api test

--- server/src/main/java/server/server/post/repository/PostRepository.java
+++ server/src/main/java/server/server/post/repository/PostRepository.java
@@ -1,4 +1,14 @@
 package server.server.post.repository;
 
-public class PostRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import server.server.post.entity.Post;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Override
+    @Query(value = "SELECT * FROM post p", nativeQuery = true)
+    List<Post> findAll();
 }
--- server/src/main/java/server/server/post/service/PostService.java
+++ server/src/main/java/server/server/post/service/PostService.java
@@ -1,4 +1,21 @@
 package server.server.post.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import server.server.post.entity.Post;
+import server.server.post.repository.PostRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    public List<Post> findPosts() {
+        return postRepository.findAll();
+    }
 }
