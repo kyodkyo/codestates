365c3f8741940efa35cd21a45ba7f19cfc130670
kyodkyo <dkekry@naver.com>
2022-08-31 00:21:05 +0900

    main-page api refactoring

--- server/src/main/java/server/server/post/controller/PostController.java
+++ server/src/main/java/server/server/post/controller/PostController.java
@@ -2,15 +2,21 @@ package server.server.post.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import server.server.post.dto.MultiResponseDto;
+import server.server.post.dto.PostResponseDto;
 import server.server.post.entity.Post;
 import server.server.post.mapper.PostMapper;
 import server.server.post.service.PostService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -22,18 +28,26 @@ public class PostController {
     private final PostService postService;
     private final PostMapper mapper;
 
+    @GetMapping
+    public ResponseEntity getPosts(@RequestParam int page, @RequestParam int size){
+        Page<Post> pagePosts = postService.findPosts(page, size);
+        List<Post> posts = pagePosts.getContent();
 
-    @GetMapping("/")
-    public ResponseEntity getPosts(){
-        List<Post> posts = postService.findPosts();
-
-//        for(Post post : posts){
-//            System.out.println(post.getContents());
-//        }
-
-//        List<PostResponseDto> postResponseDtoList = new ArrayList<>();
-
-        return new ResponseEntity(mapper.postsToPostResponseDtos(posts), HttpStatus.OK);
+        return new ResponseEntity<>(new MultiResponseDto<>(posts, pagePosts), HttpStatus.OK);
     }
 
 }
+
+
+
+/**
+ *
+ *
+        @GetMapping("/questions")
+        public ResponseEntity getPosts(){
+            List<Post> posts = postService.findPosts();
+    //        List<PostResponseDto> postResponseDtoList = new ArrayList<>();
+
+            return new ResponseEntity(posts, HttpStatus.OK);
+        }
+ */
